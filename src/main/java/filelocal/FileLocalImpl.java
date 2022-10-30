25366d2b5481cca24a915c0950151c8e73de91de
Andreja Dikic <adikic3420rn@raf.rs>
2022-10-30 11:18:49 +0100

    inicialise

---
+++ src/main/java/filelocal/FileLocalImpl.java
@@ -0,0 +1,4 @@
+package filelocal;
+
+public class FileLocalImpl {
+}
---
+++ src/main/java/filelocal/Main.java
@@ -0,0 +1,11 @@
+package filelocal;
+
+
+import Data.MyFile;
+
+public class Main {
+    private MyFile myFile;
+    public static void main(String[] args) {
+
+    }
+}
